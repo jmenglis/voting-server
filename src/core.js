369165a58f65517403741daade0c94dfdf720d77
Josh English <josh@joshenglish.com>
2016-07-12 12:38:50 -0500

    Adding the winner stipulations

--- src/core.js
+++ src/core.js
@@ -6,12 +6,19 @@ export function setEntries(state, entries) {
 
 export function next(state) {
   const entries = state.get('entries')
-  return state.merge({
-    vote: Map({
-      pair: entries.take(2)
-    }),
-    entries: entries.skip(2)
-  })
+                       .concat(getWinners(state.get('vote')))
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first())
+  } else {
+    return state.merge({
+      vote: Map({
+        pair: entries.take(2)
+      }),
+      entries: entries.skip(2)
+    })
+  }
 }
 
 export function vote(state, entry) {
